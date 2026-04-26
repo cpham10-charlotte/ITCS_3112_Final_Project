4df502b3c737feb42b320baaf451c31bc2a1b591
Colin Pham <cpham10@charlotte.edu>
2026-04-26 11:12:03 -0400

    IRecommendationService added and implemented

--- Services/RecipeCreationService.cs
+++ Services/RecipeCreationService.cs
@@ -1,6 +1,26 @@
-﻿namespace ITCS_3112_Final_Project.Services;
+﻿using ITCS_3112_Final_Project.Contracts;
+using ITCS_3112_Final_Project.Domain;
+using ITCS_3112_Final_Project.Enums;
 
-public class RecipeCreationService
+namespace ITCS_3112_Final_Project.Services;
+
+public class RecipeCreationService : IRecipeCreationService
 {
-    
+    private IRecipeRepository _recipeRepo;
+    private IIngredientRepository _ingredientRepo;
+
+    public RecipeCreationService(IRecipeRepository recipeRepo, IIngredientRepository ingredientRepo)
+    {
+        _recipeRepo = recipeRepo;
+        _ingredientRepo = ingredientRepo;
+    }
+
+    public Recipe CreateRecipe(User user, string name, string id, VeganEnum vegan, List<Ingredient> ingredients)
+    {
+        Recipe newRecipe = new Recipe(name, id, vegan, ingredients);
+        
+        _recipeRepo.Add(newRecipe);
+
+        return newRecipe;
+    }
 }
