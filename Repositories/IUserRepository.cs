c38790f8aa7462e98c4aa762cebfa097372415b2
Colin Pham <cpham10@charlotte.edu>
2026-04-26 10:27:41 -0400

    IUserRepository added

--- Repositories/IUserRepository.cs
+++ Repositories/IUserRepository.cs
@@ -1,6 +1,10 @@
 ﻿namespace ITCS_3112_Final_Project.Repositories;
 
+using ITCS_3112_Final_Project.Domain;
+
 public interface IUserRepository
 {
-    
+    User? GetUserById(string id);
+
+    void AddUser(User user);
 }
