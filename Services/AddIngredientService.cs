37861de8d61da1c60d953ccfab7597bfb5028501
Colin Pham <cpham10@charlotte.edu>
2026-05-06 11:01:58 -0400

    Match

--- Services/AddIngredientService.cs
+++ Services/AddIngredientService.cs
@@ -1,6 +1,33 @@
-﻿namespace ITCS_3112_Final_Project.Services;
+﻿using ITCS_3112_Final_Project.Contracts;
+using ITCS_3112_Final_Project.Domain;
 
-public class AddIngredientService
+namespace ITCS_3112_Final_Project.Services;
+
+public class AddIngredientService : PantryService
 {
-    
+    private readonly IPantryRepository _pantryRepo;
+    private readonly IIngredientRepository _ingredientRepo;
+
+    public AddIngredientService(IPantryRepository pantryRepo, IIngredientRepository ingredientRepo)
+    {
+        _pantryRepo = pantryRepo;
+        _ingredientRepo = ingredientRepo;
+    }
+
+    public override IReadOnlyList<Ingredient> ViewPantry(string userId)
+    {
+        var pantry = _pantryRepo.GetByUserId(userId);
+        return pantry?.GetIngredients() ?? new List<Ingredient>();
+    }
+
+    public override void UpdatePantry(string userId, string ingredientId)
+    {
+        var pantry = _pantryRepo.GetByUserId(userId) ?? new Pantry(userId);
+
+        var ingredient = _ingredientRepo.GetById(ingredientId);
+        if (ingredient == null) return;
+
+        pantry.AddIngredient(ingredient);
+        _pantryRepo.Save(pantry);
+    }
 }
